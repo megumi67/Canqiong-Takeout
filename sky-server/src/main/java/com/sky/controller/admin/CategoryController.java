569e8f97557412ce763aa885fb8ab6c52e29f64a
megumi22224 <16832890+megumi22224@user.noreply.gitee.com>
2026-05-26 20:41:34 +0800

    修改分类查询返回值显示不正确问题

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -100,7 +100,7 @@ public class CategoryController {
     @GetMapping("/list")
     @ApiOperation("根据类型查询分类")
     public Result<List<Category>> selectByType(@RequestParam Integer type) {
-          log.info("需要查询的分类为：{}",type);
+        log.info("需要查询的分类为：{}",type);
         List<Category> categoryList = categoryService.selectByType(type);
         return Result.success(categoryList);
     }
